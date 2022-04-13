4da398b232c6dcb84607178c4e9709bd2a44ab98
vlkrya <95353760+vlkrya@users.noreply.github.com>
2022-04-13 21:14:29 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ITMO.DevFound2022.FirstCSApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, remainder;
+            Console.WriteLine("enter an integer value");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            remainder = number % 2;
+            if (remainder == 0)
+                Console.WriteLine("{0} is Even", number);
+            else
+                Console.WriteLine("{0} is Odd", number);
+            Console.WriteLine("Press any key to exit ");
+            Console.ReadKey();
+        }
+    }
+}
